c0081c33fe250c4d249e70aeca6f73af748e35d6
cosamicalculadora <trabajossoporte@essatomarketing.com>
2023-09-26 09:27:45 -0600

    26092023

--- story_content/user.js
+++ story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6aL7aGwfkz8":
+      case "5zaWQa5HgV0":
         Script1();
         break;
   }
@@ -26,40 +26,32 @@ let monthlyInterestRate = (interestRate / 100) / 12;
 let comisionp = (comision / 100);
 let pagocomision = comisionp * loanAmount;
 
-// Calculate monthly and total payment using formula for loan payment
+// Calculate monthly declining balance payment using the provided formula (Cuota Fija)
 let numerator = loanAmount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, term);
 let denominator = Math.pow(1 + monthlyInterestRate, term) - 1;
-let monthlyPayment = numerator / denominator;
+let monthlyPayment1 = numerator / denominator;
 
-let pagoTotFrances = ((numerator / denominator) * term) + pagocomision ;
+// Calculate total payments for declining balance (Cuota Fija)
+let pagoTotFrances = (monthlyPayment1 * term) + pagocomision;
 
-// Calculate monthly declining balance payment using formula for loan payment
-let numerator1 = loanAmount / term ;
-let denominator1 = loanAmount * monthlyInterestRate ;
-let monthlyPayment1 = (numerator1 + denominator1) ;
+// Calculate monthly declining balance payment using the formula you provided (Cuota Sobre Saldos)
+let numerator2 = loanAmount * Math.pow(1 + (monthlyInterestRate / term), term);
+let monthlyPayment2 = numerator2 / term;
 
-let cuota1 = (loanAmount / term) + (loanAmount * monthlyInterestRate) ;
-let cuotan = (loanAmount / term) + ((loanAmount / term) * monthlyInterestRate) ;
-let pagoTotSaldos = ((cuota1 + cuotan) * (term / 2)) + pagocomision ;
-
+// Calculate total payments for declining balance (Cuota Sobre Saldos)
+let pagoTotSaldos = (monthlyPayment2 * term) + pagocomision;
 
 // Display monthly fixed payment to user
-player.SetVar("payment", + monthlyPayment.toFixed(0));
+player.SetVar("payment1", +monthlyPayment1.toFixed(2)); // Se redondea a dos decimales
 
-// Display monthly payment to user
-player.SetVar("payment1", + monthlyPayment1.toFixed(0));
+// Display total payment for Declining Balance (Cuota Fija) to user
+player.SetVar("PagoTotFrances", +pagoTotFrances.toFixed(2)); // Se redondea a dos decimales
 
-// Display total payment to user
-player.SetVar("PagoTotFrances", + pagoTotFrances.toFixed(0));
-player.SetVar("PagoTotSaldos", + pagoTotSaldos.toFixed(0));
+// Display monthly declining balance payment to user (Cuota Sobre Saldos)
+player.SetVar("payment2", +monthlyPayment1.toFixed(2)); // Se redondea a dos decimales
 
-
-// alert("Pago Mensual del préstamo (Cuota Fija): $" + monthlyPayment.toFixed(2));
-// alert("Pago Mensual del préstamo (Sobre Saldos): $" + monthlyPayment1.toFixed(2));
-// alert("Pago Total del préstamo Sobre Saldos: $" + pagoTotSaldos.toFixed(2));
-// alert("Pago Total del préstamo Francés: $" + pagoTotFrances.toFixed(2));
-// alert("CUOTA1: $" + cuota1.toFixed(2));
-// alert("CUOTAN: $" + cuotan.toFixed(2));
+// Display total payment for Declining Balance (Cuota Sobre Saldos) to user
+player.SetVar("PagoTotSaldos", +pagoTotSaldos.toFixed(2)); // Se redondea a dos decimales
 
 }
 
